4e4d4bc3ee310075740612435eef47cbb93d24c4
Michael <mlogarusic@tsn.at>
2020-10-07 12:21:05 +0200

    SQL angepasst für JDBC Aufgaben

--- src/main/java/org/openjfx/JDBC.java
+++ src/main/java/org/openjfx/JDBC.java
@@ -7,9 +7,9 @@ public class JDBC {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Test","root","");
         Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("select * from Produkte");
+        ResultSet rs = stmt.executeQuery("SELECT * FROM Invoice");
         while(rs.next())
-            System.out.println(rs.getInt(1)+" "+rs.getString(2));
+            System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4)+" "+rs.getString(5));
         con.close();
 
     }
